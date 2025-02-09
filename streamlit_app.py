5df204f76a32e42a4857529b921b12d3159f04a0
Faizal Abdillah <83067226+CoFaiz@users.noreply.github.com>
2025-02-09 20:42:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,81 +5,23 @@ import streamlit as st
 st.title('🎈 Attrition Prediction')
 st.write('Hello world!')
 
-# Importing Toolkits
-import pandas as pd
-import numpy as np
-import plotly.express as px
+st.info('This is app builds a machine learning model!')
 
-# Importing Dash Components
-from dash import Dash, html, dcc, Input, Output, dash_table
-import dash_bootstrap_components as dbc
+with st.expander('Data'):
+  st.write('**Attrition Dataset**')
+  df = pd.read_csv('https://github.com/CoFaiz/Bytebrains/blob/master/dataset_final_bytebrains_stage2.csv')
+  df
 
-used_color = ["#ADA2FF", "#C0DEFF", "#FCDDB0", "#FF9F9F", "#EDD2F3", "#98EECC", "#79E0EE"]
-# ----------- Loading Dataset -----------
-df = pd.read_csv("dataset_final_bytebrains_stage2.csv")
-df.head()
+  st.write('**X**')
+  X_raw = df.drop('StockOptionLevel', axis=1)
+  X_raw
 
-# -------------- Start The Dash App ------------------ #
-app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
 
-# To render on web app
-server = app.server
-
-# Pages Navigator
-pages_dict = {
-    "Home": "/",
-    "Departments": "/Departments",
-    "Locations": "/Locations",
-    "Performance": "/Performance",
-}
-
-# Sidebar Style
-sidebar_style = {
-    "position": "fixed",
-    "width": "16rem",
-    "height": "100vh",
-    "top": "0",
-    "bottom": "0",
-    "left": "0",
-    "padding": "15px",
-    "background-color": "#111",
-    "border-right": "2px solid #5FBDFF"
-}
-
-# Page Content Style
-content_style = {
-    "margin-left": "16rem",
-    "margin-right": "0rem",
-    "padding": "15px",
-    "height": "100%",
-}
-
-# DropDown Filter Style
-filter_style = {
-    "border-width": "0px",
-    "font-family": "arial",
-    "margin-bottom": "25px",
-    "background-color": "#222",
-}
-
-def get_alert(year_value, department_value):
-    return dbc.Alert(
-        [
-            html.H2("Warning", style={"font": "bold 30px arial"}),
-            html.P(
-                f"The Department {department_value} Did Not Exist In {year_value} !!😔😔",
-                style={"font": "bold 22px arial"}
-            ),
-            html.Hr(),
-            html.P(
-                "Choose Another Department",
-                style={"font": "bold 20px arial"},
-                className="mb-0",
-            ),
-        ], color="danger",
-        style={"box-shadow": "none", "text-shdow": "none"}
-    )
-
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='Attrition', y='count' )
 
 
 
